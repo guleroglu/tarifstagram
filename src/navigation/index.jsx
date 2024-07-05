36a1dcd3dae89065abbdcfb0736445bbe7580061
guleroglu <must60yyy@gmail.com>
2024-07-05 03:31:52 +0300

    update navigationStack

---
+++ src/navigation/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UserStack from "./UserStack";
+import AuthStack from "./AuthStack";
+import Loader from "../components/shared/Loader";
+import { useSelector } from "react-redux";
+
+export default function NavigationStack() {
+  const { loader } = useSelector((state) => state.general);
+  const isLogin = false;
+  return (
+    <>
+      {isLogin ? <UserStack /> : <AuthStack />}
+      {loader && <Loader loader={loader} />}
+    </>
+  );
+}
