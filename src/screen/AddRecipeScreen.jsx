1e0c71ad7462346897d9fffe92eb43caceb61306
guleroglu <must60yyy@gmail.com>
2024-07-06 03:22:45 +0300

    added Screen

---
+++ src/screen/AddRecipeScreen.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function AddRecipeScreen() {
+  return (
+    <View>
+      <Text>AddRecipeScreen</Text>
+    </View>
+  );
+}
