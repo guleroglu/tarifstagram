11a56ddd95e06734c25aa94bffe7e5a7551074c5
guleroglu <must60yyy@gmail.com>
2024-07-06 03:22:11 +0300

    updated Screen

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,5 +1,17 @@
 import SplashScreen from "./SplashScreen";
 import LoginScreen from "./LoginScreen";
 import RegisterScreen from "./RegisterScreen";
+import HomeScreen from "./HomeScreen";
+import AddRecipeScreen from "./AddRecipeScreen";
+import FavoriteRecipes from "./FavoriteRecipes";
+import ProfileScreen from "./ProfileScreen";
 
-export { SplashScreen, LoginScreen, RegisterScreen };
+export {
+  SplashScreen,
+  LoginScreen,
+  RegisterScreen,
+  HomeScreen,
+  AddRecipeScreen,
+  FavoriteRecipes,
+  ProfileScreen,
+};
