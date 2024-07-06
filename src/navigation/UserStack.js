3dbbfb7dcb184f2bcc5ba830b89d761cff2ccced
guleroglu <must60yyy@gmail.com>
2024-07-06 03:20:48 +0300

    updated UserStack

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -1,10 +1,24 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  HomeScreen,
+  AddRecipeScreen,
+  ProfileScreen,
+  FavoriteRecipes,
+} from "../screen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function UserStack() {
   return (
-    <View>
-      <Text>UserStack</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Favorites" component={FavoriteRecipes} />
+        <Tab.Screen name="AddRecipe" component={AddRecipeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
