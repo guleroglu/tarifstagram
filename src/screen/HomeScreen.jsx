91481097fe25c93d6a77948d50610f6be40c8adf
guleroglu <must60yyy@gmail.com>
2024-07-07 05:09:11 +0300

    updated HomeScreen

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,10 +1,13 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import Header from "../components/home/Header";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1">
+      <Header />
+      <Text>Main</Text>
+    </SafeAreaView>
   );
 }
