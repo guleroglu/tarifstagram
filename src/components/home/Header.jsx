b9e54e0f0a8deb7efbecb961f3c2c16da7c1f901
guleroglu <must60yyy@gmail.com>
2024-07-07 05:08:04 +0300

    added Home component

---
+++ src/components/home/Header.jsx
@@ -0,0 +1,30 @@
+import { Modal, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
+export default function Header() {
+
+  return (
+    <View className="bg-white flex-row justify-between items-center p-[10px]">
+      <View>
+        <Text className="text-[20px] text-primary tracking-widest">
+          tarfistagram
+        </Text>
+      </View>
+      <View className="flex-row items-center gap-2">
+        <TouchableOpacity>
+          <AntDesign name="pluscircleo" size={20} color="#00012C" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View className="relative w-[36px] h-[36px] items-center justify-center rounded-full">
+            <FontAwesome5 name="bell" size={20} color="#00012C" />
+            <View className="absolute top-1 right-1 bg-[#ff3200] justify-center items-center w-[13px] h-[13px] rounded-full">
+              <Text className="text-[10px] text-white">1</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      
+    </View>
+  );
+}
