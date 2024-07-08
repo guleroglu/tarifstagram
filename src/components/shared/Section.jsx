a228a6f496f1aad3682a2f219cd4b325c7e08cdf
guleroglu <must60yyy@gmail.com>
2024-07-08 07:20:06 +0300

    added Section

---
+++ src/components/shared/Section.jsx
@@ -0,0 +1,6 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function Section({ children }) {
+  return <View className="mt-5 mx-[10px]">{children}</View>;
+}
