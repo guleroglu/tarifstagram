c67bc2c7b30f7d035dd450727b82dcd5b8b9b4c6
guleroglu <must60yyy@gmail.com>
2024-07-08 07:21:00 +0300

    updated HomeScreen

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,13 +1,16 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import Header from "../components/home/Header";
+import RecipeList from "../components/home/RecipeList";
 
 export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView className="bg-white flex-1">
-      <Header navigation={navigation} />
-      <Text>Main</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header navigation={navigation} />
+        <RecipeList />
+      </ScrollView>
     </SafeAreaView>
   );
 }
