ab46dfcfb058f584e53190fb92a95d6746ea45a8
guleroglu <must60yyy@gmail.com>
2024-07-10 03:48:18 +0300

    added AddStepsScreen.jsx

---
+++ src/screen/AddStepsScreen.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function AddStepsScreen() {
+  return (
+    <View>
+      <Text>AddStepsScreen</Text>
+    </View>
+  );
+}
