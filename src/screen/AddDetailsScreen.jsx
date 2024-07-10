728d0f2ed6c7f83b4e48e2be7da6a938b2d96ac2
guleroglu <must60yyy@gmail.com>
2024-07-10 03:47:18 +0300

    added AddDetailsScreen.jsx

---
+++ src/screen/AddDetailsScreen.jsx
@@ -0,0 +1,10 @@
+import {  Text, View } from 'react-native'
+import React from 'react'
+
+export default function AddDetailsScreen() {
+  return (
+    <View>
+      <Text>AddDetailsScreen</Text>
+    </View>
+  )
+}
