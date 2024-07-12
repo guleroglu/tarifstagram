a334216e26606174ddd90531db83afa03480f76e
guleroglu <must60yyy@gmail.com>
2024-07-12 05:22:32 +0300

    added AddDetails.jsx

---
+++ src/components/addRecipe/AddDetails.jsx
@@ -0,0 +1,31 @@
+import { ScrollView, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AddDetailsInput from "./AddDetailsInput";
+
+export default function AddDetails() {
+  const [recipeName, setRecipeName] = useState("");
+  const [recipeSummary, setRecipeSummary] = useState("");
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView contentContainerStyle={{ padding: 20 }}>
+        <AddDetailsInput
+          label="Tarif Adı*"
+          placeholder="Tarif adı giriniz."
+          value={recipeName}
+          onChangeText={setRecipeName}
+          maxLength={55}
+        />
+        <AddDetailsInput
+          label="Tarif Özeti*"
+          placeholder="Tarif özetini giriniz."
+          value={recipeSummary}
+          onChangeText={setRecipeSummary}
+          maxLength={300}
+          multiline
+          textAlignVertical="top"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
