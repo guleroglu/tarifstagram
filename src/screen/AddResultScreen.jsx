44b15c37b217d225cf05b43b09e2bdf3e7ffb265
guleroglu <must60yyy@gmail.com>
2024-07-16 02:11:01 +0300

    The name of the AddStepsScreen became the address AddResultScreen

---
+++ src/screen/AddResultScreen.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function AddResultScreen() {
+  return (
+    <View>
+      <Text>AddResultScreen</Text>
+    </View>
+  );
+}
