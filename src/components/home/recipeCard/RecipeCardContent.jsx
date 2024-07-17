e17f7bf4acd750012ef67d70fda5ebf015d5b864
guleroglu <must60yyy@gmail.com>
2024-07-18 02:00:13 +0300

    updated RecipeCardContent.jsx

--- src/components/home/recipeCard/RecipeCardContent.jsx
+++ src/components/home/recipeCard/RecipeCardContent.jsx
@@ -4,19 +4,19 @@ import React from "react";
 export default function RecipeCardContent() {
   return (
     <View className="mb-6">
-      <View className="mb-4 gap-4">
-        <Text className="text-[24px] font-semibold">Tokat Kebabı</Text>
-        <Text className="text-[13px]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
-          vitae accusantium deleniti quae possimus atque hic ullam praesentium
-          similique quaerat.
-        </Text>
-      </View>
       <View>
         <Image
           className="w-full h-[300px] object-cover rounded-md"
           source={require("../../../../assets/tokat-kebabi-tarifi.jpg")}
         />
+      </View>
+      <View className="mt-4">
+        <Text className="text-[24px] font-semibold mb-4">Tokat Kebabı</Text>
+        <Text className="text-[13px]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+          vitae accusantium deleniti quae possimus atque hic ullam praesentium
+          similique quaerat.
+        </Text>
       </View>
     </View>
   );
