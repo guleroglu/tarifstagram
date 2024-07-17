86a2220ee5e8b04ad4d35421daf65e264a5a45cb
guleroglu <must60yyy@gmail.com>
2024-07-18 02:00:52 +0300

    updated RecipeCardUserInfo.jsx

--- src/components/home/recipeCard/RecipeCardUserInfo.jsx
+++ src/components/home/recipeCard/RecipeCardUserInfo.jsx
@@ -8,10 +8,12 @@ export default function RecipeCardUserInfo() {
         className="w-10 h-10 rounded-full"
         source={require("../../../../assets/user.png")}
       />
-      <Text className="text-primary text-[14px] font-semibold">
-        Mustafa Güleroğlu
-      </Text>
-      <Text className="text-secondary">@guleroglu</Text>
+      <View className="flex-col items-start">
+        <Text className="text-primary text-[14px] font-semibold">
+          Mustafa Güleroğlu
+        </Text>
+        <Text className="text-secondary">@guleroglu</Text>
+      </View>
     </View>
   );
 }
