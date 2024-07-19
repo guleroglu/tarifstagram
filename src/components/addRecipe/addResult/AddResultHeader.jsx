89bf765758aca19df9dd37fbb08becf8aa3bafbc
guleroglu <must60yyy@gmail.com>
2024-07-19 04:26:35 +0300

    updated AddResultHeader.jsx

---
+++ src/components/addRecipe/addResult/AddResultHeader.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+import RecipeCardUserInfo from "../../home/recipeCard/RecipeCardUserInfo";
+
+export default function AddResultHeader() {
+  return (
+    <View className="mb-4">
+      <RecipeCardUserInfo />
+    </View>
+  );
+}
