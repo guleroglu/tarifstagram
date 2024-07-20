07e6fe7f1a658f347f2559d05163f7231b69b029
guleroglu <must60yyy@gmail.com>
2024-07-20 15:48:39 +0300

    added RecipeCardCommentModal.jsx

---
+++ src/components/home/recipeCard/RecipeCardCommentModal.jsx
@@ -0,0 +1,30 @@
+import { Modal, Text, View } from "react-native";
+import React from "react";
+
+export default function RecipeCardCommentModal() {
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
