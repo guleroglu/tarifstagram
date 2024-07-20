06ea69e530655513d140e49157de5f18376c1586
guleroglu <must60yyy@gmail.com>
2024-07-20 03:24:10 +0300

    updated BottomTab

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -3,33 +3,39 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ProfileScreen, FavoriteRecipes } from "../screen";
 import { NavigationContainer } from "@react-navigation/native";
 import { AntDesign } from "@expo/vector-icons";
-import { Text } from "react-native";
 import AddRecipeStack from "./AddRecipeStack";
 import HomeStack from "./HomeStack";
+import { View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 export default function UserStack() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#F7F6FB",
+            height: 60,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+          },
+        }}
+      >
         <Tab.Screen
           name="HomeScreen"
           component={HomeStack}
           options={{
-            tabBarLabel: ({ focused }) =>
-              focused ? (
-                <Text className="text-[11px] text-primary font-medium">
-                  Ana Sayfa
-                </Text>
-              ) : (
-                <Text className="text-[11px] text-secondary">Ana Sayfa</Text>
-              ),
             tabBarIcon: ({ focused }) =>
               focused ? (
-                <AntDesign name="home" size={20} color="#00012C" />
+                <View className="items-center ">
+                  <View className="w-[4px] h-[4px] rounded-full bg-primary absolute bottom-[30px]"></View>
+                  <AntDesign name="home" size={24} color="#00012C" />
+                </View>
               ) : (
-                <AntDesign name="home" size={20} color="#868E96" />
+                <AntDesign name="home" size={24} color="#8392AE" />
               ),
           }}
         />
@@ -38,19 +44,14 @@ export default function UserStack() {
           name="Favorites"
           component={FavoriteRecipes}
           options={{
-            tabBarLabel: ({ focused }) =>
-              focused ? (
-                <Text className="text-[11px] text-primary font-medium">
-                  Favoriler
-                </Text>
-              ) : (
-                <Text className="text-[11px] text-secondary">Favoriler</Text>
-              ),
             tabBarIcon: ({ focused }) =>
               focused ? (
-                <AntDesign name="hearto" size={20} color="#00012C" />
+                <View className="items-center ">
+                  <View className="w-[4px] h-[4px] rounded-full bg-primary absolute bottom-[30px]"></View>
+                  <AntDesign name="hearto" size={24} color="#00012C" />
+                </View>
               ) : (
-                <AntDesign name="hearto" size={20} color="#868E96" />
+                <AntDesign name="hearto" size={24} color="#8392AE" />
               ),
           }}
         />
@@ -58,19 +59,14 @@ export default function UserStack() {
           name="AddRecipe"
           component={AddRecipeStack}
           options={{
-            tabBarLabel: ({ focused }) =>
-              focused ? (
-                <Text className="text-[11px] text-primary font-medium">
-                  Tarif Ekle
-                </Text>
-              ) : (
-                <Text className="text-[11px] text-secondary">Tarif Ekle</Text>
-              ),
             tabBarIcon: ({ focused }) =>
               focused ? (
-                <AntDesign name="plus" size={20} color="#00012C" />
+                <View className="items-center ">
+                  <View className="w-[4px] h-[4px] rounded-full bg-primary absolute bottom-[30px]"></View>
+                  <AntDesign name="plus" size={24} color="#00012C" />
+                </View>
               ) : (
-                <AntDesign name="plus" size={20} color="#868E96" />
+                <AntDesign name="plus" size={24} color="#8392AE" />
               ),
           }}
         />
@@ -78,19 +74,14 @@ export default function UserStack() {
           name="Profile"
           component={ProfileScreen}
           options={{
-            tabBarLabel: ({ focused }) =>
-              focused ? (
-                <Text className="text-[11px] text-primary font-medium">
-                  Profil
-                </Text>
-              ) : (
-                <Text className="text-[11px] text-secondary">Profil</Text>
-              ),
             tabBarIcon: ({ focused }) =>
               focused ? (
-                <AntDesign name="user" size={20} color="#00012C" />
+                <View className="items-center ">
+                  <View className="w-[4px] h-[4px] rounded-full bg-primary absolute bottom-[30px]"></View>
+                  <AntDesign name="user" size={24} color="#00012C" />
+                </View>
               ) : (
-                <AntDesign name="user" size={20} color="#868E96" />
+                <AntDesign name="user" size={24} color="#8392AE" />
               ),
           }}
         />
